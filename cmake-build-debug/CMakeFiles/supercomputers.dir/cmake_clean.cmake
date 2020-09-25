dab4aaa7df6b23b69d4cd5601f3657f8ec9271b5
Elizaveta Andronova <EAndronova@alfabank.ru>
2020-09-25 15:58:55 +0300

    Revert "delete cmaike-build-debug dir"
    This reverts commit a9bdb6f8dd897274fadb78b284510f487debb8d5.

---
+++ cmake-build-debug/CMakeFiles/supercomputers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/supercomputers.dir/MatrixGenerator.cpp.o"
+  "CMakeFiles/supercomputers.dir/MixColumnsCoherently.cpp.o"
+  "CMakeFiles/supercomputers.dir/main.cpp.o"
+  "supercomputers"
+  "supercomputers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/supercomputers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
